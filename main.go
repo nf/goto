d9d6095986564a614f7d5e4a753521d6ba726837
nf <nf@alaska.wh3rd.net>
2010-10-02 20:59:11 +1000

    initial commit

---
+++ main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"http"
+	"log"
+	"os"
+	"sync"
+)
+
+var (
+	listenAddr = flag.String("http", ":9980", "http listen address")
+	dataFile   = flag.String("file", "store.gob", "data store file name")
+	hostname   = flag.String("host", "r.nf.id.au", "http host name")
+	password   = flag.String("pass", "", "password")
+)
+
+func main() {
+	flag.Parse()
+	loadStore()
+	http.HandleFunc("/", Redirect)
+	http.HandleFunc("/add", Add)
+	http.ListenAndServe(*listenAddr, nil)
+}
+
+var (
+	store     = NewStore()
+	storeLock sync.Mutex
+)
+
+func Redirect(w http.ResponseWriter, r *http.Request) {
+	key := r.URL.Path[1:]
+	target := store.Get(key)
+	if target == "" {
+		http.NotFound(w, r)
+		return
+	}
+	http.Redirect(w, r, target, http.StatusFound)
+}
+
+func Add(w http.ResponseWriter, r *http.Request) {
+	key, target := r.FormValue("key"), r.FormValue("target")
+	if key == "" && target == "" {
+		fmt.Fprint(w, addform)
+		return
+	}
+	pw := r.FormValue("pw")
+	if pw != *password {
+		http.Error(w, "Forbidden", http.StatusForbidden)
+		return
+	}
+	if key == "" {
+		key = store.Set(target)
+	} else {
+		store.SetCustom(key, target)
+	}
+	fmt.Fprintf(w, "http://%s/%s", *hostname, key)
+	saveStore()
+}
+
+func loadStore() {
+	storeLock.Lock()
+	defer storeLock.Unlock()
+	f, err := os.Open(*dataFile, os.O_RDONLY, 0644)
+	if err != nil {
+		log.Stderrf("error opening %q: %s", *dataFile, err)
+		return
+	}
+	defer f.Close()
+	if err := store.ReadFrom(f); err != nil {
+		log.Stderrf("error reading %q:", *dataFile, err)
+	}
+}
+
+func saveStore() {
+	storeLock.Lock()
+	defer storeLock.Unlock()
+	f, err := os.Open(*dataFile, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+	if err != nil {
+		log.Stderrf("error opening %q: %s", *dataFile, err)
+		return
+	}
+	defer f.Close()
+	if err := store.WriteTo(f); err != nil {
+		log.Stderrf("error writing %q:", *dataFile, err)
+	}
+}
+
+const addform = `
+<form method="POST" action="/add">
+<input type="password" name="pw" length="10">
+<input type="text" name="target">
+<input type="text" name="key">
+<input type="submit" value="Add">
+</form>
+`
---
+++ store.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"gob"
+	"io"
+	"os"
+	"sync"
+)
+
+type Store struct {
+	urls  map[string]string
+	count int64
+	mu    sync.Mutex
+}
+
+func NewStore() *Store {
+	return &Store{urls: make(map[string]string)}
+}
+
+func (s *Store) Get(key string) string {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	return s.urls[key]
+}
+
+func (s *Store) Set(target string) string {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	var key string
+	for {
+		key = base36(s.count)
+		s.count++
+		if _, ok := s.urls[key]; !ok {
+			break
+		}
+	}
+	s.urls[key] = target
+	return key
+}
+
+func (s *Store) SetCustom(key, target string) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	s.urls[key] = target
+}
+
+func (s *Store) WriteTo(w io.Writer) os.Error {
+	e := gob.NewEncoder(w)
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	return e.Encode(s)
+}
+
+func (s *Store) ReadFrom(r io.Reader) os.Error {
+	d := gob.NewDecoder(r)
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	return d.Decode(s)
+}
+
+var base36Char = []byte("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func base36(n int64) string {
+	s := make([]byte, 20)
+	i := len(s) - 1
+	s[i] = base36Char[0]
+	for n > 0 && i >= 0 {
+		j := n % 36
+		n = (n - j) / 36
+		s[i] = base36Char[j]
+		i--
+	}
+	return string(s[i:])
+}
