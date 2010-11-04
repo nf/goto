fbae3e4fa60ed26bb143bf5abbd36574a2fb961f
Andrew Gerrand <adg@google.com>
2010-11-04 11:07:22 +1100

    added persistent store

--- main.go
+++ main.go
@@ -4,8 +4,6 @@ import (
 	"flag"
 	"fmt"
 	"http"
-	"log"
-	"os"
 	"sync"
 )
 
@@ -18,20 +16,24 @@ var (
 
 func main() {
 	flag.Parse()
-	loadStore()
+	store = NewPersistentStore(*dataFile)
 	http.HandleFunc("/", Redirect)
 	http.HandleFunc("/add", Add)
 	http.ListenAndServe(*listenAddr, nil)
 }
 
 var (
-	store     = NewStore()
+	store     Store
 	storeLock sync.Mutex
 )
 
 func Redirect(w http.ResponseWriter, r *http.Request) {
 	key := r.URL.Path[1:]
-	target := store.Get(key)
+	target, err := store.Get(key)
+	if err != nil {
+		http.Error(w, err.String(), http.StatusInternalServerError)
+		return
+	}
 	if target == "" {
 		http.NotFound(w, r)
 		return
@@ -40,58 +42,27 @@ func Redirect(w http.ResponseWriter, r *http.Request) {
 }
 
 func Add(w http.ResponseWriter, r *http.Request) {
-	key, target := r.FormValue("key"), r.FormValue("target")
-	if key == "" && target == "" {
+	target := r.FormValue("target")
+	if target == "" {
 		fmt.Fprint(w, addform)
 		return
 	}
-	pw := r.FormValue("pw")
-	if pw != *password {
+	if r.FormValue("pw") != *password {
 		http.Error(w, "Forbidden", http.StatusForbidden)
 		return
 	}
-	if key == "" {
-		key = store.Set(target)
-	} else {
-		store.SetCustom(key, target)
+	key, err := store.Put(target)
+	if err != nil { 
+		http.Error(w, err.String(), http.StatusInternalServerError)
+		return
 	}
 	fmt.Fprintf(w, "http://%s/%s", *hostname, key)
-	saveStore()
-}
-
-func loadStore() {
-	storeLock.Lock()
-	defer storeLock.Unlock()
-	f, err := os.Open(*dataFile, os.O_RDONLY, 0644)
-	if err != nil {
-		log.Printf("error opening %q: %s", *dataFile, err)
-		return
-	}
-	defer f.Close()
-	if err := store.ReadFrom(f); err != nil {
-		log.Printf("error reading %q: %s", *dataFile, err)
-	}
-}
-
-func saveStore() {
-	storeLock.Lock()
-	defer storeLock.Unlock()
-	f, err := os.Open(*dataFile, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
-	if err != nil {
-		log.Printf("error opening %q: %s", *dataFile, err)
-		return
-	}
-	defer f.Close()
-	if err := store.WriteTo(f); err != nil {
-		log.Printf("error writing %q: %s", *dataFile, err)
-	}
 }
 
 const addform = `
 <form method="POST" action="/add">
 <input type="password" name="pw" length="10">
 <input type="text" name="target">
-<input type="text" name="key">
 <input type="submit" value="Add">
 </form>
 `
--- store.go
+++ store.go
@@ -3,60 +3,148 @@ package main
 import (
 	"gob"
 	"io"
+	"log"
 	"os"
+	"rpc"
 	"sync"
 )
 
-type Store struct {
-	urls  map[string]string
+
+type Store interface {
+	Put(key string) (string, os.Error)
+	Get(key string) (string, os.Error)
+}
+
+
+type PersistentStore struct {
+	sync.Mutex
+	urls  *UrlMap
 	count int64
-	mu    sync.Mutex
+	filename string
 }
 
-func NewStore() *Store {
-	return &Store{urls: make(map[string]string)}
+func NewPersistentStore(filename string) *PersistentStore {
+	s := &PersistentStore{urls: NewUrlMap(), filename: filename}
+	if err := s.load(); err != nil {
+		log.Println("PersistentStore:", err)
+	}
+	return s
 }
 
-func (s *Store) Get(key string) string {
-	s.mu.Lock()
-	defer s.mu.Unlock()
-	return s.urls[key]
+func (s *PersistentStore) Get(key string) (string, os.Error) {
+	url, _ := s.urls.Get(key)
+	return url, nil
 }
 
-func (s *Store) Set(target string) string {
-	s.mu.Lock()
-	defer s.mu.Unlock()
+func (s *PersistentStore) Put(url string) (string, os.Error) {
 	var key string
+	s.Lock()
 	for {
 		key = genKey(s.count)
 		s.count++
-		if _, ok := s.urls[key]; !ok {
+		if _, ok := s.urls.Get(key); !ok {
 			break
 		}
 	}
-	s.urls[key] = target
-	return key
+	s.Unlock()
+	s.urls.Set(key, url)
+	err := s.save()
+	return key, err
 }
 
-func (s *Store) SetCustom(key, target string) {
-	s.mu.Lock()
-	defer s.mu.Unlock()
-	s.urls[key] = target
+func (s *PersistentStore) load() os.Error {
+	s.Lock()
+	defer s.Unlock()
+	f, err := os.Open(s.filename, os.O_RDONLY, 0644)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	return s.urls.ReadFrom(f)
 }
 
-func (s *Store) WriteTo(w io.Writer) os.Error {
+func (s *PersistentStore) save() os.Error {
+	s.Lock()
+	defer s.Unlock()
+	f, err := os.Open(s.filename, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	return s.urls.WriteTo(f)
+}
+
+
+type ProxyStore struct {
+	sync.Mutex
+	cache *UrlMap
+	client *rpc.Client
+}
+
+func NewProxyStore(addr string) *ProxyStore {
+	p := &ProxyStore{cache: NewUrlMap()}
+	// TODO: create client connection
+	return p
+}
+
+func (s *ProxyStore) Get(key string) (string, os.Error) {
+	if url, ok := s.cache.Get(key); ok {
+		return url, nil
+	}
+	var url string
+	err := s.client.Call("Store.Get", &key, &url)
+	if err == nil {
+		s.cache.Set(key, url)
+	}
+	return url, err
+}
+
+func (s *ProxyStore) Put(key string) (string, os.Error) {
+	var url string
+	err := s.client.Call("Store.Put", &key, &url)
+	if err == nil {
+		s.cache.Set(key, url)
+	}
+	return url, err
+}
+
+
+type UrlMap struct {
+	sync.Mutex
+	urls map[string]string
+}
+
+func NewUrlMap() *UrlMap {
+	return &UrlMap{urls: make(map[string]string)}
+}
+
+func (m *UrlMap) Set(key, url string) {
+	m.Lock()
+	m.urls[key] = url
+	m.Unlock()
+}
+
+func (m *UrlMap) Get(key string) (string, bool) {
+	m.Lock()
+	defer m.Unlock()
+	url, ok := m.urls[key]
+	return url, ok
+}
+
+func (m *UrlMap) WriteTo(w io.Writer) os.Error {
 	e := gob.NewEncoder(w)
-	s.mu.Lock()
-	defer s.mu.Unlock()
-	return e.Encode(s)
+	m.Lock()
+	defer m.Unlock()
+	return e.Encode(m.urls)
 }
 
-func (s *Store) ReadFrom(r io.Reader) os.Error {
+func (m *UrlMap) ReadFrom(r io.Reader) os.Error {
 	d := gob.NewDecoder(r)
-	s.mu.Lock()
-	defer s.mu.Unlock()
-	return d.Decode(s)
+	m.Lock()
+	defer m.Unlock()
+	return d.Decode(&m.urls)
 }
+
 
 var keyChar = []byte("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
