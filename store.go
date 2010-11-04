81ffa5e429b77f13a4161b68bfc572d9a4dc24c9
Andrew Gerrand <adg@google.com>
2010-11-04 16:00:44 +1100

    added deferred save

--- store.go
+++ store.go
@@ -7,7 +7,10 @@ import (
 	"os"
 	"rpc"
 	"sync"
+	"time"
 )
+
+const saveTimeout = 10e9
 
 
 type Store interface {
@@ -17,17 +20,23 @@ type Store interface {
 
 
 type PersistentStore struct {
-	sync.Mutex
-	urls  *UrlMap
-	count int64
-	filename string
+	mu          sync.Mutex
+	urls        *UrlMap
+	count       int64
+	filename    string
+	saveTrigger chan bool
 }
 
 func NewPersistentStore(filename string) *PersistentStore {
-	s := &PersistentStore{urls: NewUrlMap(), filename: filename}
+	s := &PersistentStore{
+		urls:        NewUrlMap(),
+		filename:    filename,
+		saveTrigger: make(chan bool, 100), // some headroom
+	}
 	if err := s.load(); err != nil {
 		log.Println("PersistentStore:", err)
 	}
+	go s.saveLoop()
 	return s
 }
 
@@ -40,7 +49,7 @@ func (s *PersistentStore) Get(key, url *string) os.Error {
 }
 
 func (s *PersistentStore) Put(url, key *string) os.Error {
-	s.Lock()
+	s.mu.Lock()
 	for {
 		*key = genKey(s.count)
 		s.count++
@@ -48,14 +57,13 @@ func (s *PersistentStore) Put(url, key *string) os.Error {
 			break
 		}
 	}
-	s.Unlock()
+	s.mu.Unlock()
 	s.urls.Set(*key, *url)
-	return s.save()
+	s.saveTrigger <- true
+	return nil
 }
 
 func (s *PersistentStore) load() os.Error {
-	s.Lock()
-	defer s.Unlock()
 	f, err := os.Open(s.filename, os.O_RDONLY, 0644)
 	if err != nil {
 		return err
@@ -64,9 +72,32 @@ func (s *PersistentStore) load() os.Error {
 	return s.urls.ReadFrom(f)
 }
 
+func (s *PersistentStore) saveLoop() {
+	saving := false
+	timeout := make(chan bool)
+	for {
+		select {
+		case <-s.saveTrigger:
+			if !saving {
+				go func() {
+					time.Sleep(saveTimeout)
+					timeout <- true
+				}()
+				saving = true
+			}
+		case <-timeout:
+			if err := s.save(); err != nil {
+				log.Println("PersistentStore:", err)
+			}
+			saving = false
+		}
+	}
+}
+
 func (s *PersistentStore) save() os.Error {
-	s.Lock()
-	defer s.Unlock()
+	log.Println("Saving")
+	s.mu.Lock()
+	defer s.mu.Unlock()
 	f, err := os.Open(s.filename, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
 	if err != nil {
 		return err
@@ -77,8 +108,7 @@ func (s *PersistentStore) save() os.Error {
 
 
 type ProxyStore struct {
-	sync.Mutex
-	cache *UrlMap
+	urls   *UrlMap
 	client *rpc.Client
 }
 
@@ -87,17 +117,17 @@ func NewProxyStore(addr string) *ProxyStore {
 	if err != nil {
 		log.Println("ProxyStore:", err)
 	}
-	return &ProxyStore{cache: NewUrlMap(), client: client}
+	return &ProxyStore{urls: NewUrlMap(), client: client}
 }
 
 func (s *ProxyStore) Get(key, url *string) os.Error {
-	if u, ok := s.cache.Get(*key); ok {
+	if u, ok := s.urls.Get(*key); ok {
 		*url = u
 		return nil
 	}
 	err := s.client.Call("Store.Get", key, url)
 	if err == nil {
-		s.cache.Set(*key, *url)
+		s.urls.Set(*key, *url)
 	}
 	return err
 }
@@ -105,14 +135,14 @@ func (s *ProxyStore) Get(key, url *string) os.Error {
 func (s *ProxyStore) Put(url, key *string) os.Error {
 	err := s.client.Call("Store.Put", url, key)
 	if err == nil {
-		s.cache.Set(*key, *url)
+		s.urls.Set(*key, *url)
 	}
 	return err
 }
 
 
 type UrlMap struct {
-	sync.Mutex
+	mu   sync.Mutex
 	urls map[string]string
 }
 
@@ -121,30 +151,28 @@ func NewUrlMap() *UrlMap {
 }
 
 func (m *UrlMap) Set(key, url string) {
-	m.Lock()
+	m.mu.Lock()
 	m.urls[key] = url
-	m.Unlock()
-	log.Println("Set", key, url)
+	m.mu.Unlock()
 }
 
 func (m *UrlMap) Get(key string) (string, bool) {
-	m.Lock()
+	m.mu.Lock()
 	url, ok := m.urls[key]
-	m.Unlock()
-	log.Println("Get", key, url, ok)
+	m.mu.Unlock()
 	return url, ok
 }
 
 func (m *UrlMap) WriteTo(w io.Writer) os.Error {
 	e := gob.NewEncoder(w)
-	m.Lock()
-	defer m.Unlock()
+	m.mu.Lock()
+	defer m.mu.Unlock()
 	return e.Encode(m.urls)
 }
 
 func (m *UrlMap) ReadFrom(r io.Reader) os.Error {
 	d := gob.NewDecoder(r)
-	m.Lock()
-	defer m.Unlock()
+	m.mu.Lock()
+	defer m.mu.Unlock()
 	return d.Decode(&m.urls)
 }
