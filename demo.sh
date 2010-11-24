5a3290d01c9fc0bf0b069cc0463f0617a115a9ce
Andrew Gerrand <nf@air.local>
2010-11-24 18:50:21 +1100

    make demo work on OS X without network

--- demo.sh
+++ demo.sh
@@ -1,54 +1,60 @@
 #!/bin/sh
 
+STATS=127.0.0.1:8090
+MASTER=127.0.0.1:8080
+N1=1
+N2=8
+N3=16
+
 echo "Starting up"
 cd ../stat/server
 ./stats &
 stats_pid=$!
 cd ../../goto
 sleep 1
-./goto -rpc=true -host=localhost:8080 &
+./goto -stats=$STATS -host=$MASTER -rpc=true &
 master_pid=$!
 sleep 1
-./goto -master=localhost:8080 -http=:8081 &
+./goto -stats=$STATS -host=$MASTER -master=$MASTER -http=:8081 &
 slave1_pid=$!
-./goto -master=localhost:8080 -http=:8082 &
+./goto -stats=$STATS -host=$MASTER -master=$MASTER -http=:8082 &
 slave2_pid=$!
-./goto -master=localhost:8080 -http=:8083 &
+./goto -stats=$STATS -host=$MASTER -master=$MASTER -http=:8083 &
 slave3_pid=$!
 sleep 1
 
-echo "Testing the master (n=1)"
-bench/bench -host=localhost:8080 -n=1 &
+echo "Testing the master (n=$N1)"
+bench/bench -stats=$STATS -host=$MASTER -n=$N1 &
 pid=$!
 read
 kill $pid
 
-echo "Testing the master (n=10)"
-bench/bench -host=localhost:8080 -n=10 &
+echo "Testing the master (n=$N2)"
+bench/bench -stats=$STATS -host=$MASTER -n=$N2 &
 pid=$!
 read
 kill $pid
 
-echo "Testing 1 slave (n=10)"
-bench/bench -host=localhost:8081 -n=10 &
+echo "Testing 1 slave (n=$N2)"
+bench/bench -stats=$STATS -host=127.0.0.1:8081 -n=$N2 &
 pid=$!
 read
 kill $pid
 
-echo "Testing 2 slaves (n=10)"
-bench/bench -host=localhost:8081,localhost:8082 -n=10 &
+echo "Testing 2 slaves (n=$N2)"
+bench/bench -stats=$STATS -host=127.0.0.1:8081,127.0.0.1:8082 -n=$N2 &
 pid=$!
 read
 kill $pid
 
-echo "Testing 3 slaves (n=10)"
-bench/bench -host=localhost:8081,localhost:8082,localhost:8083 -n=10 &
+echo "Testing 3 slaves (n=$N2)"
+bench/bench -stats=$STATS -host=127.0.0.1:8081,127.0.0.1:8082,127.0.0.1:8083 -n=$N2 &
 pid=$!
 read
 kill $pid
 
-echo "Testing 3 slaves (n=20)"
-bench/bench -host=localhost:8081,localhost:8082,localhost:8083 -n=20 &
+echo "Testing 3 slaves (n=$N3)"
+bench/bench -stats=$STATS -host=127.0.0.1:8081,127.0.0.1:8082,127.0.0.1:8083 -n=$N3 &
 pid=$!
 read
 kill $pid
--- talk/code/3/demo.sh
+++ talk/code/3/demo.sh
@@ -4,7 +4,7 @@ gomake
 ./goto -http=:8081 -rpc=true &
 master_pid=$!
 sleep 1
-./goto -master=localhost:8081 &
+./goto -master=127.0.0.1:8081 &
 slave_pid=$!
 echo "Running master on :8081, slave on :8080."
 echo "Visit: http://localhost:8080/add"
