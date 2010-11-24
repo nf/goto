2fdc9f5cff75a7b9ced61c40a30116d7e3568d4d
Andrew Gerrand <adg@google.com>
2010-11-24 11:18:54 +1100

    remove run3

--- run3.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-./goto -rpc=true -host=localhost:8080 &
-sleep 1
-./goto -master=localhost:8080 -http=:8081 &
-./goto -master=localhost:8080 -http=:8082 &
-./goto -master=localhost:8080 -http=:8083 &
